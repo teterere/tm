40f79654634015efc047a08aaf043353e07ef825
Ieva <ieva.stasjuka@protonmail.com>
2025-01-22 21:02:13 +0200

    Create task component.

--- resources/js/Pages/Tasks/Index.vue
+++ resources/js/Pages/Tasks/Index.vue
@@ -13,53 +13,7 @@
                     </div>
                 </div>
 
-                <div v-for="task in status.tasks" class="bg-white rounded p-3 mb-3 shadow-sm">
-                    <div class="flex justify-between">
-                        <div class="flex space-x-2">
-                        <span class="inline-flex items-center gap-x-1.5 rounded-md bg-red-100 px-1.5 py-0.5 text-xs font-medium text-red-700">
-                        <svg class="size-1.5 fill-red-500" viewBox="0 0 6 6" aria-hidden="true">
-                          <circle cx="3" cy="3" r="3"/>
-                        </svg>
-                        {{ task.priority.title }}
-                      </span>
-
-                        <div v-if="task.labels.length" class="flex items-center space-x-1">
-                            <span v-for="label in task.labels"
-                                  class="inline-flex items-center rounded-md px-1.5 py-0.5 text-xs font-medium text-blue-700"
-                                  :style="{ backgroundColor: label.color }"
-                            >
-                                <span :style="{ color: label.color, filter: 'brightness(30%)'}">{{ label.title }}</span>
-                            </span>
-                        </div>
-                        </div>
-
-                        <EllipsisHorizontalIcon class="h-4 w-4 text-gray-400" />
-                    </div>
-
-                    <div class="py-3">
-                        <h3 class="font-medium mb-2">{{ task.title }}</h3>
-                        <p class="text-gray-500 text-sm line-clamp-3">{{ task.description }}</p>
-                    </div>
-                    <div class="flex items-center border-b-1 pb-2 mb-2">
-                        <ClockIcon class="h-4 w-4 text-gray-500 mr-1" />
-                        <span class="text-gray-500 text-xs">{{ task.due_date }}</span>
-                    </div>
-                    <div class="flex justify-between items-center">
-                        <img class="inline-block size-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-
-                        <div class="flex items-center space-x-3">
-                            <div class="flex items-center">
-                                <CheckCircleIcon class="h-4 w-4 text-gray-500 mr-1" />
-                                <span class="text-sm text-gray-500">2/4</span>
-                            </div>
-
-                            <div class="flex items-center">
-                                <EnvelopeIcon class="h-4 w-4 text-gray-500 mr-1" />
-                                <span class="text-sm text-gray-500">2</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <Task v-for="task in status.tasks" :task="task" />
             </div>
         </div>
     </AppLayout>
@@ -75,6 +29,7 @@ import {
     EnvelopeIcon,
     CheckCircleIcon
 } from '@heroicons/vue/24/outline';
+import Task from "@/Components/Task/Task.vue";
 
 const props = defineProps({
     statuses: Object
