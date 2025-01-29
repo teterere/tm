45af30d27336d03cdb75aeecea5a289b576322dd
Ieva <ieva.stasjuka@protonmail.com>
2025-01-29 21:19:12 +0200

    Toggle showing input field for edit mode.

--- resources/js/Components/Task/TaskDetailsModal/TaskChecklist/TaskChecklistItem.vue
+++ resources/js/Components/Task/TaskDetailsModal/TaskChecklist/TaskChecklistItem.vue
@@ -2,19 +2,28 @@
     <div class="flex -ml-2">
         <div @mouseover="showActionButtons = true"
              @mouseleave="showActionButtons = false"
-             :class="showActionButtons ? 'bg-gray-100' : ''"
+             :class="showActionButtons || editStatus ? 'bg-gray-100' : ''"
              class="flex w-full items-center justify-between px-1">
-            <div class="flex items-center">
+            <div class="flex items-center w-full">
                 <div class="flex items-center justify-center text-gray-300 hover:text-gray-400 cursor-pointer p-2 mr-1">
                     <Bars3Icon class="h-4 w-4" />
                 </div>
                 <input @change="toggleCompleted" type="checkbox" :id="'item-' + item.id" :checked="item.completed" class="border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto mr-2" />
-                <label :for="'item-' + item.id" class="text-sm" :class="item.completed ? 'line-through text-gray-400' : 'text-gray-600'">
+                <input v-if="editStatus" type="text" v-model="form.description" class="block w-full rounded-md bg-white px-1 -ml-1 mr-1 py-0 text-base text-gray-900 placeholder:text-gray-400 sm:text-sm/6 outline-0 border-0 focus:ring-0 focus:border-0 focus:outline-0">
+                <label v-else :for="'item-' + item.id" class="text-sm" :class="item.completed ? 'line-through text-gray-400' : 'text-gray-600'">
                     {{ item.description }}
                 </label>
             </div>
-            <div v-if="showActionButtons" class="flex gap-x-1">
-                <button @mousedown.prevent="" class="bg-white hover:bg-gray-200 p-1 rounded-sm cursor-pointer">
+            <div v-if="editStatus" class="flex gap-x-1">
+                <button @mousedown.prevent="editStatus = false" class="bg-white hover:bg-gray-200 p-1 rounded-sm cursor-pointer">
+                    <XMarkIcon class="w-4 h-4" />
+                </button>
+                <button @mousedown.prevent="updateItem" class="bg-white hover:bg-gray-200 p-1 rounded-sm cursor-pointer">
+                    <CheckIcon class="w-4 h-4" />
+                </button>
+            </div>
+            <div v-else v-if="showActionButtons" class="flex gap-x-1">
+                <button @mousedown.prevent="editStatus = true" class="bg-white hover:bg-gray-200 p-1 rounded-sm cursor-pointer">
                     <PencilIcon class="w-4 h-4" />
                 </button>
                 <button @mousedown.prevent="deleteItem" class="bg-white hover:bg-gray-200 p-1 rounded-sm cursor-pointer">
@@ -27,7 +36,7 @@
 
 <script setup>
 import {router, useForm} from "@inertiajs/vue3";
-import {Bars3Icon, PencilIcon, TrashIcon} from "@heroicons/vue/24/outline/index.js";
+import {Bars3Icon, PencilIcon, TrashIcon, CheckIcon, XMarkIcon} from "@heroicons/vue/24/outline/index.js";
 import {ref} from "vue";
 
 const props = defineProps({
@@ -35,6 +44,7 @@ const props = defineProps({
 })
 
 const showActionButtons = ref(false);
+const editStatus = ref(false);
 
 const deleteItem = () => {
     router.delete(route('tasks.checklist-items.delete', { task: props.item.task_id, item: props.item.id }), {
@@ -47,6 +57,14 @@ const toggleCompleted = () => {
         preserveScroll: true
     });
 };
+
+const form = useForm({
+    description: props.item.description
+});
+
+const updateItem = () => {
+
+}
 </script>
 
 <style scoped>
