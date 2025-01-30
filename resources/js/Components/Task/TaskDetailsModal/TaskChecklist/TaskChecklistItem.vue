2fb0333a81650a51bf824ec53c24f253acc6a13b
Ieva <ieva.stasjuka@protonmail.com>
2025-01-30 22:38:14 +0200

    Autofocus input when edit mode is true.

--- resources/js/Components/Task/TaskDetailsModal/TaskChecklist/TaskChecklistItem.vue
+++ resources/js/Components/Task/TaskDetailsModal/TaskChecklist/TaskChecklistItem.vue
@@ -9,7 +9,7 @@
                     <Bars3Icon class="h-4 w-4" />
                 </div>
                 <input @change="toggleCompleted" type="checkbox" :id="'item-' + item.id" :checked="item.completed" class="border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto mr-2" />
-                <input v-if="editStatus" v-model="form.description" @keyup.enter="updateItem" type="text" class="block w-full rounded-md bg-white px-1 -ml-1 mr-1 py-0 text-base text-gray-900 placeholder:text-gray-400 sm:text-sm/6 outline-0 border-0 focus:ring-0 focus:border-0 focus:outline-0">
+                <input ref="inputRef" v-if="editStatus" v-model="form.description" @keyup.enter="updateItem" type="text" class="block w-full rounded-md bg-white px-1 -ml-1 mr-1 py-0 text-base text-gray-900 placeholder:text-gray-400 sm:text-sm/6 outline-0 border-0 focus:ring-0 focus:border-0 focus:outline-0">
                 <label v-else :for="'item-' + item.id" class="w-full cursor-pointer text-sm" :class="{'line-through text-gray-400': item.completed, 'text-gray-600': !item.completed}">
                     {{ item.description }}
                 </label>
@@ -37,19 +37,25 @@
 <script setup>
 import {router, useForm} from "@inertiajs/vue3";
 import {Bars3Icon, PencilIcon, TrashIcon, CheckIcon, XMarkIcon} from "@heroicons/vue/24/outline/index.js";
-import {ref, watch} from "vue";
+import {nextTick, ref, watch} from "vue";
 
 const props = defineProps({
     item: Object
 });
 
-const emit = defineEmits(['update:edit-status']);
+const emit = defineEmits(['edit-status-changed']);
 
 const showActionButtons = ref(false);
 const editStatus = ref(false);
+const inputRef = ref(null);
 
-watch(editStatus, (newValue) => {
+watch(editStatus, async (newValue) => {
     emit('edit-status-changed', newValue);
+
+    if (newValue === true) {
+        await nextTick();
+        inputRef.value?.focus();
+    }
 });
 
 const deleteItem = () => {
