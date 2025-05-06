7d5d1244d2864f9361b71a0731427137b602f331
Ieva <ieva.stasjuka@protonmail.com>
2025-05-06 09:47:30 +0300

    Turn email into a link.

--- resources/js/Pages/AboutDeveloper.vue
+++ resources/js/Pages/AboutDeveloper.vue
@@ -1,58 +1,61 @@
 <template>
     <AppLayout>
-        <section class="grid md:grid-cols-3 md:pl-12 py-22 space-x-12">
-            <div class="col-span-2 flex flex-col justify-center px-6">
-                <h1 class="text-5xl font-bold text-gray-900 mb-8">Par izstrādātāju</h1>
+        <div class="md:w-2/3 flex flex-col justify-center px-6 md:pl-12 md:py-22 space-x-12">
+            <h1 class="text-5xl font-bold text-gray-900 mb-8">Par izstrādātāju</h1>
 
-                <div class="space-y-18 text-gray-700">
-                    <div class="space-y-4">
-                        <p>
-                            Esmu web izstrādātāja ar vairāku gadu pieredzi backend un frontend izstrādē. Ikdienā strādāju ar Laravel, Vue un Inertia.js, veidojot risinājumus, kas ir gan funkcionāli, gan vizuāli pārdomāti.
-                        </p>
+            <div class="space-y-12 text-gray-700">
+                <div class="space-y-4">
+                    <p>
+                        Esmu web izstrādātāja ar vairāku gadu pieredzi backend un frontend izstrādē. Ikdienā strādāju ar Laravel, Vue.js un Inertia.js, veidojot risinājumus, kas ir gan funkcionāli, gan vizuāli pārdomāti.
+                    </p>
 
-                        <p>
-                            Lielāko pieredzi esmu guvusi <strong>Tet</strong> un <strong>DeskTime</strong> uzņēmumos, strādājot komandās pēc Agile un Scrum principiem.
-                        </p>
+                    <p>
+                        Lielāko pieredzi esmu guvusi <strong>Tet</strong> un <strong>DeskTime</strong> uzņēmumos, strādājot komandās pēc Agile un Scrum principiem.
+                    </p>
 
-                        <p>
-                            Man ir svarīga koda kvalitāte, saprotama struktūra, lietotāja pieredze un kopējais produkta tēls.
-                        </p>
+                    <p>
+                        Man ir svarīga koda kvalitāte, saprotama struktūra, lietotāja pieredze un kopējais produkta tēls.
+                    </p>
+                </div>
+
+
+
+                <div class="space-y-8">
+                    <div>
+                        <p class="font-semibold mb-1">Tehnoloģijas un valodas:</p>
+                        <ul class="flex items-start flex-wrap gap-2">
+                            <li v-for="title in ['PHP', 'JavaScript', 'Laravel', 'Vue.js', 'Nuxt.js', 'Joomla', 'Scss', 'BootStrap', 'Tailwind', 'Git']" class="bg-gray-200 rounded-full py-1 px-3 text-xs font-semibold">
+                                {{ title }}
+                            </li>
+                        </ul>
                     </div>
 
                     <div>
-                        <p>Esmu atvērta jaunām iespējām. Ja tev šķiet, ka mēs varētu sadarboties, sazinies ar mani!</p>
-                        <div class="flex items-center gap-2">
-                            <span class="text-lg">✉️</span>
-                            <span class="font-semibold">ieva.avota@protonmail.com</span>
-                        </div>
+                        <p class="font-semibold mb-1">Citi rīki:</p>
+                        <ul class="flex items-start flex-wrap gap-2">
+                            <li v-for="title in ['Jira', 'ClickUp', 'Figma', 'GitLab']" class="bg-gray-200 rounded-full py-1 px-3 text-xs font-semibold">
+                                {{ title }}
+                            </li>
+                        </ul>
                     </div>
+                </div>
 
-                    <div class="space-y-8">
+                <div>
+                    <div class="flex items-center gap-x-2 bg-white border pl-2 pr-4 py-4">
+                        <span class="text-4xl">👋</span>
                         <div>
-                            <p class="font-semibold mb-1">Tehnoloģijas un valodas:</p>
-                            <ul class="flex items-start flex-wrap gap-2">
-                                <li v-for="title in ['PHP', 'JavaScript', 'Laravel', 'Vue.js', 'Nuxt.js', 'Joomla', 'Scss', 'BootStrap', 'Tailwind', 'Git']" class="bg-gray-200 rounded-full py-1 px-3 text-xs font-semibold">
-                                    {{ title }}
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div>
-                            <p class="font-semibold mb-1">Citi rīki:</p>
-                            <ul class="flex items-start flex-wrap gap-2">
-                                <li v-for="title in ['Jira', 'ClickUp', 'Figma', 'GitLab']" class="bg-gray-200 rounded-full py-1 px-3 text-xs font-semibold">
-                                    {{ title }}
-                                </li>
-                            </ul>
+                            <p>Esmu atvērta jaunām iespējām. Ja tev šķiet, ka mēs varētu sadarboties, sazinies ar mani!</p>
+                            <a href="mailto:ieva.avota@protonmail.com" class="font-semibold hover:underline">
+                                ieva.avota@protonmail.com
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
     </AppLayout>
 </template>
 
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
-import { EnvelopeIcon } from '@heroicons/vue/24/outline';
 </script>
